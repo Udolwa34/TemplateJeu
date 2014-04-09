ca3b9dd5b10770f7e38b19be88eeb25a181abc0a
CorentinFagh <corentin.fagherazzi@y-nov.com>
2014-04-09 11:55:02 +0200

    Creation PileEcran

---
+++ TemplateJeu/TemplateJeu/MoteurJeu/PileEcran.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemplateJeu
+{
+    class PileEcran
+    {
+        private List<long> Ecrans;
+
+        public PileEcran() 
+        {
+            Ecrans = new List<long>();
+        }
+
+        public long getLast()
+        {
+            return Ecrans.Last();
+        }
+
+        public void empiler(long nvo)
+        {
+            Ecrans.Add(nvo);
+        }
+
+        public void depiler()
+        {
+            if (Ecrans.Count()!=0)
+            Ecrans.RemoveAt(Ecrans.Count() - 1);
+        }
+
+        public void viderPile()
+        {
+            Ecrans.Clear();
+        }
+
+        public void toMenu()
+        {
+            while (Ecrans.Last().nom != "Menu")
+            {
+                depiler();
+            }
+        }
+
+        public void draw()
+        {
+            foreach (long Scr in Ecrans)
+                if (Scr.print == true)
+                    Scr.draw();
+        }
+
+        public void update()
+        {
+            Ecrans.Last().update();
+        }
+    }
+}
+
